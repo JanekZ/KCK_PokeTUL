efef6c1a7a8696202205ef166f22e437ac46af48
Jan Zimniak <zimniak.janek@outlook.com>
2025-04-24 23:33:04 +0200

    Implemented merge of doubly linked list and stack. It will allow better movement handling.

---
+++ Algorithms/dll_stack.py
@@ -0,0 +1,51 @@
+class Node:
+    def __init__(self, value: str) -> None:
+        self.next: Node|None = None
+        self.prev: Node|None = None
+        self.value: str = value
+
+class DLLStack:
+    def __init__(self):
+        self.head = None
+
+    def push(self, value: str) -> None:
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+        else:
+            self.head.next = new_node
+            new_node.prev = self.head
+            self.head = new_node
+
+    def pop(self) -> str | None:
+        if not self.is_empty():
+            head_node_value = self.head.value
+            if self.head.prev is not None:
+                self.head.prev.next = None
+                self.head = self.head.prev
+            else:
+                self.head = None
+            return head_node_value
+        return None
+
+    def is_empty(self) -> bool:
+        return self.head is None
+
+    #Removes the node with given value that is closest to head
+    def remove(self, value: str) -> None:
+        node = self.head
+        while node is not None:
+            if node.value is value:
+                if node.prev is not None:
+                    node.prev.next = node.next
+                if node.next is None:
+                    self.head = node.prev
+                else:
+                    node.next.prev = node.prev
+                break
+            else:
+                node = node.prev
+
+
+    def top(self) -> Node:
+        return self.head
---
+++ unittests/test_movement.py
@@ -0,0 +1,97 @@
+import unittest
+from Algorithms.dll_stack import DLLStack
+from Algorithms.dll_stack import Node
+
+class TestMovement(unittest.TestCase):
+    def test_node_initialization(self):
+        value = "left"
+        node = Node(value)
+        self.assertIsNone(node.next)
+        self.assertIsNone(node.prev)
+        self.assertEqual(node.value, value)
+
+    def test_dll_stack_initialization(self):
+        dll_stack = DLLStack()
+        self.assertIsNone(dll_stack.head)
+
+    def test_empty(self):
+        dll_stack = DLLStack()
+        self.assertTrue(dll_stack.is_empty())
+
+        value = "right"
+        dll_stack.push(value)
+        self.assertFalse(dll_stack.is_empty())
+
+        dll_stack.pop()
+        self.assertTrue(dll_stack.is_empty())
+
+    def test_push(self):
+        dll_stack = DLLStack()
+
+        value = "left"
+        dll_stack.push(value)
+        self.assertEqual(value, dll_stack.top().value)
+
+        value2 = "right"
+        dll_stack.push(value2)
+        self.assertEqual(value2, dll_stack.top().value)
+
+        value3 = "top"
+        dll_stack.push(value3)
+        self.assertEqual(value3, dll_stack.top().value)
+
+    def test_pop(self):
+        dll_stack = DLLStack()
+
+        value = "left"
+        dll_stack.push(value)
+        value2 = "right"
+        dll_stack.push(value2)
+        value3 = "top"
+        dll_stack.push(value3)
+
+        self.assertEqual(value3, dll_stack.pop())
+        self.assertEqual(value2, dll_stack.pop())
+        self.assertEqual(value, dll_stack.pop())
+        self.assertIsNone(dll_stack.pop())
+
+    def test_remove(self):
+        dll_stack = DLLStack()
+
+        values = ["left", "right", "top", "down"]
+        dll_stack.push(values[0])
+        dll_stack.push(values[1])
+        dll_stack.push(values[2])
+        dll_stack.push(values[3])
+
+        key = "down"
+        dll_stack.remove(key)
+        node = dll_stack.head
+        while node is not None:
+            self.assertNotEqual(node.value, key)
+            node = node.prev
+
+        key = "left"
+        dll_stack.remove(key)
+        node = dll_stack.head
+        while node is not None:
+            self.assertNotEqual(node.value, key)
+            node = node.prev
+
+        key = "top"
+        dll_stack.remove(key)
+        node = dll_stack.head
+        while node is not None:
+            self.assertNotEqual(node.value, key)
+            node = node.prev
+
+        key = "right"
+        dll_stack.remove(key)
+        node = dll_stack.head
+        while node is not None:
+            self.assertNotEqual(node.value, key)
+            node = node.prev
+
+
+if __name__ == "__main__":
+    unittest.main()
