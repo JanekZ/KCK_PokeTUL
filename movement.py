32c572ac01e08b77cd58c9cda81c89953d21c3fe
Jan Zimniak <zimniak.janek@outlook.com>
2025-05-05 21:07:22 +0200

    Added different camera movement.

--- movement.py
+++ movement.py
@@ -3,7 +3,7 @@ from algorithms.collision_detection import CollisionDetection
 
 class Movement:
     def __init__(self, layers):
-        self.camera_mode = c.STATIC_CAMERA
+        self.camera_mode = c.MOVING_CAMERA
         self.layers = layers
         self.jump = False
         self.jump_destination = None
@@ -22,15 +22,24 @@ class Movement:
 
         is_collision_with_building, num_of_collisions = collision_detection.check_collision(self.layers[c.BUILDINGS_LAYER])
         is_out_of_bounds = collision_detection.check_out_of_bounds(self.layers[c.BACKGROUND_LAYER])
-
+        
         if is_collision_with_building or is_out_of_bounds or is_collision_with_portal:
             if self.camera_mode == c.STATIC_CAMERA:
                 for obj in self.layers[c.CHARACTERS_LAYER]:
                     obj.d_x, obj.d_y = c.MOVE_TRANSLATE[c.STOP]
+
+            if self.camera_mode == c.MOVING_CAMERA:
+                for layer in self.layers[:c.CHARACTERS_LAYER]:
+                    for obj in layer:
+                        obj.d_x, obj.d_y = c.MOVE_TRANSLATE[c.STOP]
         else:
             if self.camera_mode == c.STATIC_CAMERA:
                 for obj in self.layers[c.CHARACTERS_LAYER]:
                     obj.d_x, obj.d_y = d_xy
+            if self.camera_mode == c.MOVING_CAMERA:
+                for layer in self.layers[:c.CHARACTERS_LAYER]:
+                    for obj in layer:
+                        obj.d_x, obj.d_y = -d_xy[0], -d_xy[1]
 
     def get_updated_layers(self):
         return self.layers
