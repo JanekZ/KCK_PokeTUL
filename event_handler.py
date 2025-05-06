77fbeb4b140e9aabfb918e0411449c8b86ef5c02
Jan Zimniak <zimniak.janek@outlook.com>
2025-05-06 17:55:08 +0200

    Refactored and added comments to event_handler.py

--- event_handler.py
+++ event_handler.py
@@ -3,24 +3,49 @@ from commands import *
 
 class EventHandler:
     def __init__(self):
+        """
+        SETTING UP COMMANDS:
+            Creating class properties that represent different commands.
+        """
         self.up_command = UpCommand()
         self.down_command = DownCommand()
         self.left_command = LeftCommand()
         self.right_command = RightCommand()
 
+    def handle_events(self, state) -> None:
+        """
+        EVENT LOOP:
+            Every event sent to two different function, that check quit events and keyboard events.
 
-    def handle_events(self, state):
+        :param state: Current state that will resolve the event.
+        """
         for event in pygame.event.get():
             self.check_quit_event(event)
             self.check_keyboard_event(event, state)
 
     @staticmethod
-    def check_quit_event(event):
+    def check_quit_event(event) -> None:
+        """
+        EVENT CHECK:
+            Checking if quit event is called.
+
+        :param event: Event sent to be checked.
+        """
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
 
-    def check_keyboard_event(self, event, state):
+    def check_keyboard_event(self, event, state) -> None:
+        """
+        EVENT TYPE CHECK:
+            Checking if event type matches any of verified types.
+
+        KEY CHECK:
+            Events key is compared to all verified key to find proper command to execute.
+
+        :param event: Event sent to be checked.
+        :param state: Current state that will resolve the event.
+        """
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
                 self.up_command.execute(state, c.PUSH)
