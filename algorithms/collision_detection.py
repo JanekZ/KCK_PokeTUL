69354c02553502255029f57b04d9fd14f7dc687f
Jan Zimniak <zimniak.janek@outlook.com>
2025-05-06 17:02:07 +0200

    Refactored and added comments to collision_detection.py

--- algorithms/collision_detection.py
+++ algorithms/collision_detection.py
@@ -2,12 +2,27 @@ import pygame
 from dynamic_entity import DynamicEntity
 
 class CollisionDetection:
-    def __init__(self, obj: DynamicEntity, d_xy: tuple[int, int]):
+    def __init__(self, obj: DynamicEntity, d_xy: tuple[int, int]) -> None:
+        """
+        SETTING UP:
+            Creation of class properties using parameters.
+        CLONE:
+            Clone creation with offset of d_xy, used to check for collisions.
+
+        :param obj: Object that will be used to check for collisions.
+        :param d_xy: Change in x and y-axis.
+        """
         self.obj = obj
         self.d_x, self.d_y = d_xy
         self.clone = self.create_clone()
 
     def create_clone(self) -> DynamicEntity:
+        """
+        CREATION:
+            New dynamic entity object is created with new x and y values with offset.
+
+        :return: Clone of the object with offset.
+        """
         clone = DynamicEntity()
         new_x = self.obj.rect.x + self.d_x
         new_y = self.obj.rect.y + self.d_y
@@ -18,17 +33,38 @@ class CollisionDetection:
         return clone
 
     def check_collision(self, layer: pygame.sprite.Group, ) -> tuple[bool, int]:
+        """
+        COLLISION CHECK:
+            Pygame built-in function checks if clone of character is colliding with any object from given layer.
+
+        :param layer: Layer of objects that character can collide with.
+        :return: Tuple with boolean value representing collision and number of elements that character collided with.
+        """
         collisions = pygame.sprite.spritecollide(self.clone, layer, dokill=False)
         is_col = len(collisions) >= 1
         return is_col, len(collisions)
 
     def check_collision_with_portal(self, layer: pygame.sprite.Group, ) -> tuple:
+        """
+        COLLISION CHECK:
+            Pygame built-in function checks if clone of character is colliding with any portal object from given layer.
+
+        :param layer: Layer of objects that character can collide with.
+        :return: Tuple with boolean value representing collision and destination of given portal.
+        """
         collisions = pygame.sprite.spritecollide(self.clone, layer, dokill=False)
         is_col = len(collisions) >= 1
         destination = collisions[0].destination if is_col else None
         return is_col, destination
 
     def check_out_of_bounds(self, layer: pygame.sprite.Group, ) -> bool:
+        """
+        COLLISION CHECK:
+            Manual checking if clone of character is inside given boundary.
+
+        :param layer: Layer of bounding objects.
+        :return: Tuple with boolean value representing if character is out of bounds.
+        """
         is_col, _ = self.check_collision(layer)
         boundaries = [i for i in layer]
         boundary = boundaries[0]
