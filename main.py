06b1f3576ec1622fdf2f76aaf200b8c73143f5f2
Jan Zimniak <zimniak.janek@outlook.com>
2025-05-06 14:48:52 +0200

    Refactored and added comments to main.py.

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import setup
 import constants as c
 
-def main():
+def main() -> None:
+    """
+    SETUP FUNCTIONS:
+        Calls the setup.py functions to initialize variables responsible for correct setup of controller object.
+    CONTROLLER RUN:
+        Prompts the controller object to start the main game loop.
+    """
     setup.init()
     clock = setup.get_clock()
     display = setup.get_display()
     game_states = setup.get_game_states()
-    # sets the controller and runs the main loop of the game
     controller = setup.get_controller(display, clock, game_states, c.MAIN_SCREEN)
     controller.run()
 
 
 if __name__ == "__main__":
+    """
+    FUNCTION CALL:
+        Calls the main function only if the python file is run directly.
+    """
     main()
