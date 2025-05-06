aef549aae6a8ddf8c90bc175d0cb7a7485d3144c
Jan Zimniak <zimniak.janek@outlook.com>
2025-05-06 15:40:55 +0200

    Refactored and added comments to commands.py.

--- commands.py
+++ commands.py
@@ -1,5 +1,13 @@
 import constants as c
-
+"""
+COMMANDS:
+    Classes that represent key press actions with execute method.
+EXECUTE:
+    Class method that calls game state function to process user input.
+    
+    :param actor: Game state object used for access to its methods.
+    :param action: Text variable that represents what type of action has to be executed.
+"""
 class UpCommand:
     def execute(self, actor, action):
         actor.user_input(action, c.UP)
